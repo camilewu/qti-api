64a2b4605ffa82661990a33dc6690874c99e0973
camilewu <camilewu@openknowledge.hk>
2024-09-02 09:58:58 +0800

    put media inside itembody but before type of questions

--- index.js
+++ index.js
@@ -2,225 +2,231 @@ const express = require('express');
 const xmlbuilder = require('xmlbuilder');
 const archiver = require('archiver');
 const fs = require('fs-extra');
+const axios = require('axios');
+const cors = require('cors');
 const path = require('path');
 const cron = require('node-cron');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const PORT = 3000;
 const TEMP_DIR = path.join(__dirname, 'temp');
 
+app.use(cors());
 app.use(express.json());
 
 function jsonToQtiXml(question) {
   const xmlRoot = xmlbuilder.create('assessmentItem', { version: '1.0', encoding: 'UTF-8' })
     .att('xmlns', 'http://www.imsglobal.org/xsd/imsqti_v2p1')
-    .att('xsi:schemaLocation', 'http://www.imsglobal.org/xsd/imsqti_v2p1 http://www.imsglobal.org/xsd/qti/qtiv2p1/imsqti_v2p1.xsd')
+    .att('xsi:schemaLocation', 'http://www.imsglobal.org/xsd/imsqti_v2p1 http://www.imsglobal.org/xsd/qti/qtiv2p1.xsd')
     .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
     .att('identifier', question.douid || 'default_id')
     .att('title', question.title || 'Default Title')
     .att('adaptive', 'false')
-    .att('timeDependent', 'false');
+    .att('timeDependent', 'false')
+    .att('xml:lang', 'en');
 
-  const itemBody = xmlRoot.ele('itemBody');
-  let interactionElement;
-  let needsDefaultResponse = true;
+      // 添加responseDeclaration 和 outcomeDeclaration
+    xmlRoot.ele('responseDeclaration', {
+      identifier: 'RESPONSE',
+      cardinality: 'single',
+      baseType: 'identifier'
+    }).ele('correctResponse').ele('value', {}, '');
 
+    xmlRoot.ele('outcomeDeclaration', {
+      identifier: 'SCORE',
+      cardinality: 'single',
+      baseType: 'float'
+    }).ele('defaultValue').ele('value', {}, '');
+
+  // 創建 itemBody 
+    const itemBody = xmlRoot.ele('itemBody');
+    // const mediaElement = itemBody.ele('div');
+     const pElement = itemBody.ele('div');
+
+    // 將媒體文件放在itembody裏面但在題型之前
+    if (question.asset && question.asset.files) {
+      for (const file of question.asset.files) {
+        if (file.url) {
+          const fileName = file.name.toLowerCase();
+          const fileSavePath = path.posix.join(fileName);
+          console.log('File save path:', fileSavePath);
+
+          if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg') || fileName.endsWith('.gif') || fileName.endsWith('.png')) {
+            pElement.ele('img', { src: fileSavePath, alt: 'Image' });
+          } else if (fileName.endsWith('.mp3')) {
+            const audioElement = pElement.ele('audio', { controls: 'controls' });
+            audioElement.ele('source', { src: fileSavePath, type: 'audio/mpeg' });
+            // Optionally add fallback text for browsers that do not support the audio element
+            audioElement.txt('Your browser does not support the audio element.');
+          }
+
+          // if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg')) {
+          //   mediaElement = itemBody.ele('object', { type: 'image/jpeg', data: fileSavePath });
+          // } else if (fileName.endsWith('.png')) {
+          //   mediaElement = itemBody.ele('object', { type: 'image/png', data: fileSavePath });
+          // } else if (fileName.endsWith('.gif')) {
+          //   mediaElement = itemBody.ele('object', { type: 'image/gif', data: fileSavePath });
+          // } else if (fileName.endsWith('.mp3')) {
+          //   mediaElement = itemBody.ele('object', { type: 'audio/mpeg', data: fileSavePath });
+          //   console.log('Audio object created:', mediaElement.name);
+          //   mediaElement.ele('param', { name: 'autoplay', value: 'false' });
+          //   mediaElement.ele('param', { name: 'controls', value: 'true' });
+          // }
+        }
+      }
+    }
+    
+
+  // 問題類型
   switch (question.type) {
     case 'QBTextMC':
       const responseId = `RESPONSE_${question.douid}`;
-    
-      interactionElement = itemBody.ele('choiceInteraction', {
+      const interactionElement = itemBody.ele('choiceInteraction', {
         responseIdentifier: responseId,
-        shuffle: 'true',
+        shuffle: 'false',
         maxChoices: '1'
       });
-    
       interactionElement.ele('prompt', {}, question.question);
-    
+
       let correctOptionIndex = null;
       let optionIndex = 1;
-    
       for (let i = 1; i <= question.totalOptions; i++) {
         const optionKey = `opt${i}`;
         const optionValue = question[optionKey];
-    
         if (optionValue) {
           interactionElement.ele('simpleChoice', { identifier: `OPTION_${optionIndex}` }, optionValue);
-    
           if (i.toString() === question.answer) {
             correctOptionIndex = `OPTION_${optionIndex}`;
           }
           optionIndex++;
         }
       }
-    
       if (correctOptionIndex) {
         const responseDeclaration = xmlRoot.ele('responseDeclaration', {
           identifier: responseId,
           cardinality: 'single',
           baseType: 'identifier'
         });
-    
         const correctResponse = responseDeclaration.ele('correctResponse');
         correctResponse.ele('value', {}, correctOptionIndex);
-        needsDefaultResponse = false;
       } else {
         console.warn(`Correct option not found for question ID: ${question.douid}`);
       }
       break;
-    case 'QBTrueFalse':    
-      interactionElement = itemBody.ele('choiceInteraction', {
+
+    case 'QBTrueFalse':
+      const trueFalseInteraction = itemBody.ele('choiceInteraction', {
         responseIdentifier: `RESPONSE_${question.douid}`,
-        shuffle: 'true',
+        shuffle: 'false',
         maxChoices: '1'
       });
-    
-      interactionElement.ele('prompt', {}, question.question);
-     
+      trueFalseInteraction.ele('prompt', {}, question.question);
       const trueFalseResponseDeclaration = xmlRoot.ele('responseDeclaration', {
         identifier: `RESPONSE_${question.douid}`,
         cardinality: 'single',
         baseType: 'boolean',
       });
       const trueFalseResponse = trueFalseResponseDeclaration.ele('correctResponse');
-      let trueFalseCorrectAnswer = question.answer.toString();
-      trueFalseResponse.ele('value', {}, trueFalseCorrectAnswer);
-    
-      interactionElement.ele('simpleChoice', { identifier: '1' }, '正確');
-      interactionElement.ele('simpleChoice', { identifier: '0' }, '錯誤');
-      interactionElement.ele('simpleChoice', { identifier: '1' }, 'True');
-      interactionElement.ele('simpleChoice', { identifier: '0' }, 'False');
-    
-      //   if(trueFalseCorrectAnswer === '1'){
-      //     trueFalseCorrectAnswer  ==='正確';
-      //   }else if(trueFalseCorrectAnswer === '0'){
-      //     trueFalseCorrectAnswer ==='錯誤';
-      //   }
-      //   interactionElement.ele('simpleChoice', { identifier: `${trueFalseCorrectAnswer}` }, trueFalseCorrectAnswer);
-      needsDefaultResponse = false;
+      trueFalseResponse.ele('value', {}, question.answer.toString());
+
+      let reg = /[\u4E00-\u9FFF]|[\u3400-\u4DBF]|[\uF900-\uFAFF]|\u3000|[\uFE30-\uFE4F]|[\u3000-\u303F]|\u3000|\u3003|\u3008-\u3011|\u3014|\u3015|\u301C-\u301E|[\uFF01-\uFF5E]/;
+      if (question.question.match(reg)) {
+        trueFalseInteraction.ele('simpleChoice', { identifier: '1' }, '正確');
+        trueFalseInteraction.ele('simpleChoice', { identifier: '0' }, '錯誤');
+      } else {
+        trueFalseInteraction.ele('simpleChoice', { identifier: '1' }, 'True');
+        trueFalseInteraction.ele('simpleChoice', { identifier: '0' }, 'False');
+      }
       break;
-
-    // case 'QBRecorder':
-    // case 'QBTakePhoto':
-      // interactionElement = itemBody.ele('uploadInteraction', {
-      //   responseIdentifier: `RESPONSE_${question.douid}`
-      // });
-
-      // interactionElement.ele('prompt', {}, question.question);
-      // break;
 
     case 'QBShortAnswer':
     case 'QBLongQuestion':
-      interactionElement = itemBody.ele('extendedTextInteraction', {
+      itemBody.ele('extendedTextInteraction', {
         responseIdentifier: `RESPONSE_${question.douid}`,
         expectedLength: "200"
-      });
-
-      interactionElement.ele('prompt', {}, question.question);
+      }).ele('prompt', {}, question.question);
 
       const shortAnswerResponseDeclaration = xmlRoot.ele('responseDeclaration', {
         identifier: `RESPONSE_${question.douid}`,
         cardinality: 'single',
         baseType: 'string'
       });
-
       const shortAnswerCorrectResponse = shortAnswerResponseDeclaration.ele('correctResponse');
-      let extend_correctAnswer = question.answer.toString();
-      shortAnswerCorrectResponse.ele('value', {}, extend_correctAnswer);
-      needsDefaultResponse = false;
+      shortAnswerCorrectResponse.ele('value', {}, question.answer.toString());
       break;
 
     case 'QBFillingBlank':
-      interactionElement = itemBody.ele('textEntryInteraction', {
+      const fillingBlank = itemBody.ele('extendedTextInteraction', {
         responseIdentifier: `RESPONSE_${question.douid}`,
         expectedLength: "200"
       });
-    
-      interactionElement.ele('prompt', {}, question.question);
-      // let prompt = question.question.match()
-    
+
+      let correctQ = question.question.replace(/\[.*?\]/g, "____");
+      fillingBlank.ele('prompt', {}, correctQ);
+
       const fillingBlankResponseDeclaration = xmlRoot.ele('responseDeclaration', {
         identifier: `RESPONSE_${question.douid}`,
         cardinality: 'single',
         baseType: 'string'
       });
-    
       const fillingBlankCorrectResponse = fillingBlankResponseDeclaration.ele('correctResponse');
-      let correctAnswer = question.question.match(/\[(.*?)\]/)[1];
+      const correctAnswer = question.question.match(/\[(.*?)\]/)[1];
       fillingBlankCorrectResponse.ele('value', {}, correctAnswer);
-      needsDefaultResponse = false;
+      break;
+
+    case 'QBInfoBlock':
+      itemBody.ele('extendedTextInteraction', {
+        responseIdentifier: `RESPONSE_${question.douid}`,
+        expectedLength: "200"
+      }).ele('prompt', {}, question.question);
       break;
 
     case 'QBToggleOptions':
-      interactionElement = itemBody.ele('hotspotInteraction', {
+      itemBody.ele('hotspotInteraction', {
         responseIdentifier: `RESPONSE_${question.douid}`,
         maxChoices: "1"
-      });
+      }).ele('prompt', {}, question.question);
+      break;
 
-      interactionElement.ele('prompt', {}, question.question);
+    case 'QBRecorder':
+    case 'QBTakePhoto':
+      itemBody.ele('uploadInteraction', {
+        responseIdentifier: `RESPONSE_${question.douid}`
+      }).ele('prompt', {}, question.question);
       break;
 
     case 'QBDragLine':
     case 'DragDropA':
     case 'DragDropC':
-      interactionElement = itemBody.ele('associateInteraction', {
+      itemBody.ele('associateInteraction', {
         responseIdentifier: `RESPONSE_${question.douid}`,
-        shuffle: "true",
+        shuffle: "false",
         maxAssociations: "3"
-      });
-
-      interactionElement.ele('prompt', {}, question.question);
+      }).ele('prompt', {}, question.question);
       break;
 
     default:
       console.warn(`Unknown question type: ${question.type}`);
   }
-  
-  if (question.asset && question.asset.files) {
-    question.asset.files.forEach(file => {
-      if (file.url) {
-        const url = `https://oka.blob.core.windows.net/media/${file.url}`;
-        const fileName = file.name.toLowerCase();
-
-        if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg') || fileName.endsWith('.gif') || fileName.endsWith('.png')) {
-          interactionElement.ele('img', { src: url });
-        } else if (fileName.endsWith('.mp3')) {
-          interactionElement.ele('audio', { controls: '' }).ele('source', { src: url, type: 'audio/mpeg' });
-        }
-      } 
-    });
-  }
-
-  if (needsDefaultResponse) {
-    const responseDeclaration = xmlRoot.ele('responseDeclaration', {
-      identifier: `RESPONSE_${question.douid}`,
-      cardinality: 'single',
-      baseType: 'string'
-    });
-
-    responseDeclaration.ele('correctResponse').ele('value', {}, 'N/A');
-  }
 
   return xmlRoot.end({ pretty: true });
 }
 
-
-
-function createManifest(questionFiles) {
+function createManifest(questionFiles, lang = 'en') {
   const manifest = xmlbuilder.create('manifest', { version: '1.0', encoding: 'UTF-8' })
     .att('xmlns', 'http://www.imsglobal.org/xsd/imscp_v1p1')
     .att('xmlns:imsmd', 'http://www.imsglobal.org/xsd/imsmd_v1p2')
     .att('xmlns:imsqti', 'http://www.imsglobal.org/xsd/imsqti_v2p1')
     .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
-    .att('identifier', 'MANIFEST-QTI-1')
+    .att('identifier', `MANIFEST-QTI-${uuidv4()}`)
     .att('xsi:schemaLocation', 'http://www.imsglobal.org/xsd/imscp_v1p1 imscp_v1p1.xsd http://www.imsglobal.org/xsd/imsmd_v1p2 imsmd_v1p2p2.xsd http://www.imsglobal.org/xsd/imsqti_v2p1 imsqti_v2p1.xsd');
 
   manifest.ele('organizations');
-
   const resources = manifest.ele('resources');
-
+//媒體文件
   questionFiles.forEach((file, index) => {
     const resource = resources.ele('resource', {
-      identifier: `qti_item_${index + 1}`,
+      identifier: `qti_item_${index + 1}_${uuidv4()}`, 
       type: 'imsqti_item_xmlv2p1',
       href: file
     });
@@ -232,27 +238,11 @@ function createManifest(questionFiles) {
     const lom = metadata.ele('imsmd:lom');
     const general = lom.ele('imsmd:general');
     general.ele('imsmd:identifier', `qti_v2_item_${index + 1}`);
+
     const title = general.ele('imsmd:title');
-    title.ele('imsmd:langstring', { 'xml:lang': 'en' }, `Metadata Example Item #${index + 1}`);
+    title.ele('imsmd:langstring', { 'xml:lang': lang }, `这是示例题目 #${index + 1}`);
     const description = general.ele('imsmd:description');
-    description.ele('imsmd:langstring', { 'xml:lang': 'en' }, 'This is a dummy item');
-
-    const lifecycle = lom.ele('imsmd:lifecycle');
-    const version = lifecycle.ele('imsmd:version');
-    version.ele('imsmd:langstring', { 'xml:lang': 'en' }, '1.0');
-    const status = lifecycle.ele('imsmd:status');
-    const source = status.ele('imsmd:source');
-    source.ele('imsmd:langstring', { 'xml:lang': 'x-none' }, 'LOMv1.0');
-    const value = status.ele('imsmd:value');
-    value.ele('imsmd:langstring', { 'xml:lang': 'x-none' }, 'Draft');
-
-    const qtiMetadata = metadata.ele('imsqti:qtiMetadata');
-    qtiMetadata.ele('imsqti:timeDependent', 'false');
-    qtiMetadata.ele('imsqti:feedbackType', 'nonadaptive');
-    qtiMetadata.ele('imsqti:solutionAvailable', 'true');
-    qtiMetadata.ele('imsqti:toolName', 'XMLSPY');
-    qtiMetadata.ele('imsqti:toolVersion', '5.4');
-    qtiMetadata.ele('imsqti:toolVendor', 'ALTOVA');
+    description.ele('imsmd:langstring', { 'xml:lang': lang }, '这是一个示例题目的描述');
 
     resource.ele('file', { href: file });
   });
@@ -260,29 +250,58 @@ function createManifest(questionFiles) {
   return manifest.end({ pretty: true });
 }
 
+
+const manifestContent = createManifest(['question1.xml', 'question2.xml'], 'zh');
+// console.log(manifestContent);
+
+
 app.post('/convert', async (req, res) => {
   try {
     const questionFiles = [];
-    
-    req.body.questions.forEach((question, index) => {
+    const subfolderName = `qti_content_${Date.now()}`;
+    const subfolderPath = path.join(TEMP_DIR, subfolderName);
+
+    await fs.ensureDir(subfolderPath);
+
+    const promises = req.body.questions.map(async (question, index) => {
       const xmlOutput = jsonToQtiXml(question);
-      const fileName = `assessment_item_${index + 1}.xml`;
-      fs.writeFileSync(path.join(TEMP_DIR, fileName), xmlOutput);
+      const fileName = `assessment_item_${String(index + 1).padStart(3, '0')}.xml`;
+      const filePath = path.join(subfolderPath, fileName);
+
+      await fs.writeFile(filePath, xmlOutput);
       questionFiles.push(fileName);
+
+      if (question.asset && question.asset.files) {
+        const filePromises = question.asset.files.map(async file => {
+          if (file.url) {
+            const fullUrl = `https://oka.blob.core.windows.net/media/${file.url}`;
+            const fileName = file.name.toLowerCase();
+            const mediaFileName = path.join(subfolderPath,  fileName);
+            await fs.ensureDir(path.dirname(mediaFileName));
+            const response = await axios.get(fullUrl, { responseType: 'arraybuffer' });
+            await fs.writeFile(mediaFileName, response.data);
+            console.log(`Saved file: ${mediaFileName}`);
+          }
+        });
+        await Promise.all(filePromises);
+      }
     });
+
+    await Promise.all(promises);
 
     const manifestOutput = createManifest(questionFiles);
     const manifestFileName = 'imsmanifest.xml';
-    fs.writeFileSync(path.join(TEMP_DIR, manifestFileName), manifestOutput);
+    await fs.writeFile(path.join(subfolderPath, manifestFileName), manifestOutput);
 
-    const zipFileName = `qti_${Date.now()}.zip`;
+    const zipFileName = `${subfolderName}.zip`;
     const zipFilePath = path.join(TEMP_DIR, zipFileName);
     const output = fs.createWriteStream(zipFilePath);
     const archive = archiver('zip');
 
-    output.on('close', () => {
+    output.on('close', async () => {
       console.log(`ZIP file ${zipFileName} created successfully.`);
-      res.download(zipFilePath);
+      await res.download(zipFilePath);
+      await fs.remove(subfolderPath);
     });
 
     archive.on('error', err => {
@@ -290,26 +309,29 @@ app.post('/convert', async (req, res) => {
     });
 
     archive.pipe(output);
-    questionFiles.forEach(file => {
-      archive.file(path.join(TEMP_DIR, file), { name: file });
-    });
-    archive.file(path.join(TEMP_DIR, manifestFileName), { name: manifestFileName });
-    archive.finalize();
+    archive.directory(subfolderPath, false);
+    await archive.finalize();
   } catch (error) {
-    res.status(500).send('Error converting JSON to XML: ' + error);
+    console.error('Error converting JSON to XML:', error);
+    res.status(500).send('Error converting JSON to XML: ' + error.message);
   }
 });
 
 cron.schedule('0 0 * * *', async () => {
-  const files = await fs.readdir(TEMP_DIR);
-  const now = Date.now();
-  for (const file of files) {
-    const filePath = path.join(TEMP_DIR, file);
-    const stats = await fs.stat(filePath);
-    if ((now - stats.mtimeMs) > 7 * 24 * 60 * 60 * 1000) {
-      await fs.unlink(filePath);
-      console.log(`Deleted old file: ${file}`);
-    }
+  try {
+    const files = await fs.readdir(TEMP_DIR);
+    const now = Date.now();
+    const deletionPromises = files.map(async file => {
+      const filePath = path.join(TEMP_DIR, file);
+      const stats = await fs.stat(filePath);
+      if ((now - stats.mtimeMs) > 7 * 24 * 60 * 60 * 1000) { // 文件超過7天會被自動刪除
+        await fs.unlink(filePath);
+        console.log(`Deleted old file: ${file}`);
+      }
+    });
+    await Promise.all(deletionPromises);
+  } catch (error) {
+    console.error('Error during cron job execution:', error);
   }
 });
 
@@ -326,4 +348,10 @@ app.use(express.static("./public"));
 
 app.use(function(req, res, next) {
   res.status(404).send('Page not found');
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
